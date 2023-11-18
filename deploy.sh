ec43994ed530fcc8809d93b5f9b986c286b80866
sagar <sagar@raacom.com>
2023-11-18 15:51:39 +0530

    modified

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 flutter build web
-cp -fr build/web/web/* /home/sagar/StudioProjects/asset_zone_web_host/
+cp -fr build/web/* /home/sagar/StudioProjects/asset_zone_web_host/
 git commit -m "modified" .
 git push git@github.com:asagarshinde/asset_zone_web_host.git
